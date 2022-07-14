27d3c3f2a8233c02854556a65ea3325466a371c7
kichukegor <kichukegor1997@gmail.com>
2022-07-14 16:43:00 +0300

    fix: fast fix fast fix with eslint

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import React from 'react';
-import { MoralisProvider } from 'react-moralis';
 import Searcher from './common/components/searher/Searcher';
 import CardList from './common/components/cardList/CardList';
 import { OpenseaApiProvider } from './hooks/useOpenseaApi';
 import './App.css';
 
-const { DAPP_URL, APPLICATION_ID } = process.env;
-
 function App() {
   return (
-    <MoralisProvider serverUrl={DAPP_URL} appId={APPLICATION_ID}>
-      <div className="App">
-        <OpenseaApiProvider>
-          <Searcher placeholder="Your address" />
-          <CardList />
-        </OpenseaApiProvider>
-      </div>
-    </MoralisProvider>
+    <div className="App">
+      <OpenseaApiProvider>
+        <Searcher placeholder="Your address" />
+        <CardList />
+      </OpenseaApiProvider>
+    </div>
   );
 }
 
